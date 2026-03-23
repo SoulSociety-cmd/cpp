49038b5217228cab051e085d838c3216192662f2
Soul女 <lmtkhang2007@gmail.com>
2026-03-23 16:52:55 +0700

    Update main.cpp

--- Largest Rectangle in Histogram/main.cpp
+++ Largest Rectangle in Histogram/main.cpp
@@ -1,26 +1,31 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 int n,i,j,r,l,s;
-int a[10000+2];
 
 int main()
 {
     cin >> n;
-    for (i=1;i<=n;i++) cin >> a[i];
-    for (i=1;i<=n;i++){
-            for (j=i-1;j>=0;j--){
-                if (a[j] < a[i]){
-                    l=j;
-                    break;
-                }
+    vector<int>a(n);
+    for (i=0;i<n;i++) cin >> a[i];
+    for (i=0;i<n;i++)
+    {
+        for (j=i-1;j>=0;j--)
+        {
+            if (a[j] < a[i])
+            {
+                l=j;
+                break;
             }
-            for (j=i+1;j<=n+1;j++){
-                if (a[j] < a[i]){
-                    r=j;
-                    break;
-                }
+        }
+        for (j=i+1;j<=n+1;j++)
+        {
+            if (a[j] < a[i])
+            {
+                r=j;
+                break;
             }
+        }
         s=max(s,a[i]*(r-l-1));
     }
     cout << s;
