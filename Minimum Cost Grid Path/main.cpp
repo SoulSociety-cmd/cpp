3dd3ffa63b8ef821f6307046523d61d3f75b19ed
Soul女 <lmtkhang2007@gmail.com>
2026-03-24 17:36:56 +0700

    Add files via upload

---
+++ Minimum Cost Grid Path/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+int a[100][100],cost[100][100],dp[100][100];
+using namespace std;
+void snt(int hang,int cot,int a[100][100])
+{
+    for(int i=0;i<hang;i++)
+    for(int j=0;j<cot;j++)
+    for(int k=2;k<=a[i][j];k++)
+    if (a[i][j]%k==0) cost[i][j]+=1;
+}
+
+int main()
+{
+    int n,m;cin>>m>>n;
+    for(int i=0;i<m;i++)
+    for(int j=0;j<n;j++) cin >> a[i][j];
+    snt(m,n,a);dp[0][0]=cost[0][0];
+    for(int i=0;i<m;i++)
+    for(int j=0;j<n;j++)
+        dp[i][j]=min(dp[i][j-1]+cost[i][j],dp[i-1][j]+cost[i][j]);
+    cout<<dp[m-1][n-1];
+}
