a5feb27d38e5c9a21ae9c6a0296713bb8c232a98
Soul女 <lmtkhang2007@gmail.com>
2026-03-24 19:44:38 +0700

    Update main.cpp

--- Minimum Cost Grid Path/main.cpp
+++ Minimum Cost Grid Path/main.cpp
@@ -2,24 +2,40 @@
 
 using namespace std;
 
-void snt(int hang,int cot,int a[100][100])
+void snt(int hang,int cot,vector<vector<int>> &a,vector<vector<int>> &cost)
 {
     for(int i=0;i<hang;i++)
-    for(int j=0;j<cot;j++)
-    for(int k=2;k<=a[i][j];k++)
-    if (a[i][j]%k==0) cost[i][j]+=1;
+        {
+            for(int j=0;j<cot;j++)
+                {
+                    for(int k=1;k<=a[i][j];k++)
+                        {
+                            if(a[i][j] % k == 0) cost[i][j]++;
+                        }
+                }
+        }
 }
 
 int main()
 {
-    vector<vector<int,int>> cost,dp;
-    int n,m;cin>>m>>n;
-    vector<vector<int,int>> a(n,vector<int>(m));
-    for(int i=0;i<m;i++)
-    for(int j=0;j<n;j++) cin >> a[i][j];
-    snt(m,n,a);dp[0][0]=cost[0][0];
-    for(int i=0;i<m;i++)
-    for(int j=0;j<n;j++)
-        dp[i][j]=min(dp[i][j-1]+cost[i][j],dp[i-1][j]+cost[i][j]);
-    cout<<dp[m-1][n-1];
+    int n,m;cin>>n>>m;
+    vector<vector<int>> a(n,vector<int>(m));
+     vector<vector<int>> cost(n,vector<int>(m));
+     vector<vector<int>> dp(n,vector<int>(m));
+    for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++) cin >> a[i][j];
+        }
+    snt(n,m,a,cost);dp[0][0]=cost[0][0];
+    for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+                {
+                    if(i==0 && j==0) continue;
+                    if(i==0) dp[i][j] = dp[i][j-1] + cost[i][j];
+                    else if(j==0) dp[i][j] = dp[i-1][j] + cost[i][j];
+                    else dp[i][j] = min(dp[i][j] = dp[i-1][j] ,dp[i][j-1] ) + cost[i][j];
+                }
+        }
+    cout<<dp[n-1][m-1];
 }
